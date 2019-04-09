007551748df7e279e97039b3737adb52ec4d49d0
NdumisoMagunga <magunga55@gmail.com>
2019-04-09 04:53:17 -0700

    the app folders structure set-up done

---
+++ src/components/AppNavbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AppNAvbar extends Component {
+  render() {
+    return (
+      <div className="container">
+        <text>this is the navbar!!</text>
+      </div>
+    );
+  }
+}
+
+export default AppNAvbar;
